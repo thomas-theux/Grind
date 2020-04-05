6e091dd60b751146963aaadae796beb9e04ff75a
Thomas Theux <t.grikschas@gmail.com>
2020-04-05 10:50:46 +0200

    Added basics

---
+++ Grinder/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerController : MonoBehaviour {
+
+    public int playerID = 0;
+
+    public GameObject BoardGO;
+
+    public float directionValue = 0.0f;
+    public float addedPush = 0.1f;
+    public float smooth = 5.0f;
+
+    private Player player;
+
+    // REWIRED
+    private bool dPadLeft = false;
+    private bool dPadRight = false;
+
+
+    private void Awake() {
+        player = ReInput.players.GetPlayer(playerID);
+    }
+
+
+    private void Update() {
+        GetInput();
+        ProcessInput();
+        BoardRotation();
+    }
+
+
+    private void GetInput() {
+        dPadLeft = player.GetButton("Left");
+        dPadRight = player.GetButton("Right");
+    }
+
+
+    private void ProcessInput() {
+        if (dPadLeft) {
+            directionValue -= addedPush;
+        }
+        
+        if (dPadRight) {
+            directionValue += addedPush;
+        }
+    }
+
+
+    private void BoardRotation() {
+        BoardGO.transform.Rotate(0, 0, -directionValue);
+    }
+
+}
